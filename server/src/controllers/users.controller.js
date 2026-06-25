cab3a71e27b191e46a6555af7b9f81819b78ea93
Manthan Srivastava <manthanks.0606@gmail.com>
2026-06-25 23:45:04 +0530

    change the http cookie option config

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -24,7 +24,7 @@ const resend = new Resend(process.env.RESEND_VERIFICATION_MAIL_API_KEY);
 const options = {
   httpOnly: true,
   secure: true,
-  sameSite: "None",
+  sameSite: "Strict",
 };
 
 // user OTP generator method
